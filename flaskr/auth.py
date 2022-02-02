47e38ec2da902f52e7dd99ad0a8a90e90eb4ad77
Forrest Deters <forrest.deters@gmail.com>
2022-02-01 21:48:33 -0800

    added login functionality and html for auth

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -25,18 +25,77 @@ def register():
         elif not password:
             error = 'Password is required.'
         
-        # try to log in using provided credentials
+        # try to register using provided credentials
         if error is None:
             try:
                 db.execute(
                     "INSERT INTO user (username, password) VALUES (?, ?)",
                     (username, generate_password_hash(password)),
                 )
+                db.commit()
+            # handle user already registered
             except db.IntegrityError:
                 error = f"User {username} is already registered."
             else:
                 return redirect(url_for('auth.login'))
         
-        flash(error)  # flashes an error in the browser, if one exists
+        flash(error)  # stores a message to put in the template, if one exists
     
     return render_template('auth/register.html')
+
+
+@bp.route('/login', methods=('GET', 'POST'))
+def login():
+    # handle POST requests, when a user attempts to login (submit form)
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+        error = None
+        user = db.execute(
+            'SELECT * FROM user WHERE username = ?', (username,)
+        ).fetchone()  # returns one row from the query, as opposed to fetchall
+
+        # handle wrong username or password
+        if user is None:
+            error = 'Incorrect username.'
+        elif not check_password_hash(user['password'], password):
+            error = 'Incorrect password.'
+        
+        if error is None:
+            session.clear()
+            session['user_id'] = user['id']
+            return redirect(url_for('index'))
+        
+        flash(error)
+    
+    return render_template('auth/login.html')
+
+
+@bp.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('index'))
+
+
+@bp.before_app_request
+def load_logged_in_user():
+    user_id = session.get('user_id')
+
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = get_db().execute(
+            'SELECT * FROM user WHERE id = ?', (user_id,)
+        ).fetchone()
+
+
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for('auth.login'))
+        
+        return view(**kwargs)
+    
+    return wrapped_view
